08e7e103328eb99cc3a36e9795bf7159cba43657
Thoa Duong <kimthoa2598@gmail.com>
2021-01-01 08:45:54 +0800

    done tempplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
       "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+        extend: {
+            gridTemplateRows: {
+                '10': 'repeat(10, minmax(0, 1fr))',
+            },
+            gridRow: {
+                'span-7': 'span 7 / span 7',
+                'span-8': 'span 8 / span 8',
+                'span-9': 'span 9 / span 9',
+                'span-10': 'span 10 / span 10',
+            },
+            lineHeight: {
+                '14': '3.5rem',
+            },
+            animation: {
+                marquee: 'marquee 25s linear infinite',
+                marquee2: 'marquee2 25s linear infinite',
+            },
+            keyframes: {
+                marquee: {
+                    '0%': { transform: 'translateX(0%)' },
+                    '100%': { transform: 'translateX(-100%)' },
+                },
+                marquee2: {
+                    '0%': { transform: 'translateX(100%)' },
+                    '100%': { transform: 'translateX(0%)' },
+                },
+            },
+        }
     },
     plugins: [],
   }
